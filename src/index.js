1706d83f5f19fbb26d685acb63cb8d38aee96675
Nat209 <nata.yuri2003@gmail.com>
2024-08-07 13:30:18 -0500

    UI/UX

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import personasRoutes from './routes/personas.routes.js'
+
+// Inicialización
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Configuración
+app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
+app.engine('hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(__dirname, 'views', 'layouts'),
+    partialsDir: join(__dirname, 'views', 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');  
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Rutas
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use(personasRoutes)
+
+// Archivos estáticos
+app.use(express.static(join(__dirname, 'public')));
+
+// Ejecutar el servidor
+app.listen(app.get('port'), () => {
+    console.log('El servidor está escuchando en el puerto', app.get('port'));
+});
